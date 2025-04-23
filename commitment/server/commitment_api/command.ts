ae2595750e9668dc8417c854ae8a0e9f00d56f9b
Nicholas Bisset <nbis0006@student.monash.edu>
2025-04-23 18:09:11 +1000

    Scaffold Code for Git API fixing #1
    changed the exec functionality so that you can execute in a certain file path

--- commitment/server/commitment_api/command.ts
+++ commitment/server/commitment_api/command.ts
@@ -19,23 +19,25 @@ export const doNotLogData: Command = {
     shouldLog: false
 }
 
-export const executeCommand = (f: Command): Promise<string> => {
+export const executeCommand = (cwd: string) => (f: Command): Promise<string> => {
     return new Promise((resolve, reject) => {
-        exec(f.cmd, (error: Error | null, stdout: string, stderr: string) => {
+        exec(f.cmd, 
+            { cwd: cwd },
+            (error: Error | null, stdout: string, stderr: string) => {
 
-            if (stderr) { 
-                if (f.shouldLog) f.onStdFail(stderr)
-                return reject(stderr);
+                if (stderr) { 
+                    if (f.shouldLog) f.onStdFail(stderr)
+                    return reject(stderr);
 
-            } else if (error) {
-                if (f.shouldLog) f.onFail(error)
-                return reject(error);
+                } else if (error) {
+                    if (f.shouldLog) f.onFail(error)
+                    return reject(error);
 
-            } else {
-                if (f.shouldLog) f.onSuccess()
-                return resolve(stdout)
+                } else {
+                    if (f.shouldLog) f.onSuccess()
+                    return resolve(stdout)
+                }
             }
-
-        })
+        )
     })
 }
