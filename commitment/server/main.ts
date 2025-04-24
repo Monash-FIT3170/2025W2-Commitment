6a2fad37725a5d590c897337c98aee2eaec73123
ishwiz <154478861+ishwiz@users.noreply.github.com>
2025-04-24 23:59:38 +1000

    fix: ensured linting requirements are met

--- commitment/server/main.ts
+++ commitment/server/main.ts
@@ -31,7 +31,7 @@ Meteor.startup(async () => {
 
   // We publish the entire Links collection to all clients.
   // In order to be fetched in real-time to the clients
-  Meteor.publish("links", function () {
+  Meteor.publish('links', function () {
     return LinksCollection.find();
   });
 });
