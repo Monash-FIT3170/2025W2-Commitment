9c31089589908a112ca86ab368a1abe6edf1d2d5
Bailey Chessum <52596359+BaileyChessum@users.noreply.github.com>
2025-04-29 14:24:07 +1000

    Add TailwindCSS and shadcn/ui (#8)
    * feat: working tailwind setup
    
    * feat: working shadcnui, with example usage
    
    * fix: remove .ts extension from shadcn utils path
    
    * update: package-lock.json

--- commitment/imports/ui/App.tsx
+++ commitment/imports/ui/App.tsx
@@ -3,9 +3,11 @@ import { Hello } from './Hello';
 import { Info } from './Info';
 
 export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
+  <div className="m-9">
+    <h1 className="text-cyan-700 font-sans text-5xl m-9 text-center font-thin">Welcome to Meteor!</h1>
+    <div className="flex flex-col gap-6">
+      <Hello />
+      <Info />
+    </div>
   </div>
 );
