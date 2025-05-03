b77f676d4e36747a97a4e47d9eb8ba217d6e92ca
Amy T <amycaitjea@gmail.com>
2025-05-03 22:16:55 +1000

    Created an orange card

--- commitment/imports/ui/App.tsx
+++ commitment/imports/ui/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Hello } from './Hello';
 import { Info } from './Info';
+import { Card, CardContent, CardHeader } from './components/ui/card';
+import { Menu } from 'lucide-react';
 
 export const App = () => (
   <div className="m-9">
     <h1 className="text-cyan-700 font-sans text-5xl m-9 text-center font-thin">Welcome to Meteor!</h1>
     <div className="flex flex-col gap-6">
-      <Hello />
+      <Card  className='bg-[#F1502F]'>
+      <CardHeader>
+          title 
+          <Menu />
+        </CardHeader>
+        <CardContent>
+          hihi
+        </CardContent>
+    </Card>
+    <Hello />
       <Info />
     </div>
   </div>
