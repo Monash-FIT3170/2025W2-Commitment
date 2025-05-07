896d74dca939de7e0375ebfff5e3c66f3ee6f78f
ubat0001 <ubat0001@student.monash.edu>
2025-05-07 21:55:18 +1000

    feat: adding footer components to landing page

--- commitment/imports/ui/LandingPage.tsx
+++ commitment/imports/ui/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './Footer';
 
 export const LandingPage = () => (
   <div className="m-9">
     <h1 className="text-cyan-700 font-sans text-5xl m-9 text-center font-thin">Commitment</h1>
     <div className="flex flex-col gap-6">
         {/* Put components here */}
+        < Footer />
     </div>
   </div>
 );
