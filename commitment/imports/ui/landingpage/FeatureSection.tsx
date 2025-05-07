d4afc096bca34d44682f50cf531f35b00f611f3b
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-05-07 15:47:46 +1000

    feat: Display the title text.

---
+++ commitment/imports/ui/landingpage/FeatureSection.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export function FeatureSection() {
+
+    return (
+        <div>
+            
+            <div className="w-full text-center mt-10">
+                {/* Section for mini title */}
+                <p className="text-sm text-[#F1502F] font-medium font-robotoFlex tracking-wide">
+                    OUR FEATURES?
+                </p>
+                {/* Section for big description */}
+                <h1 className="text-5xl text-gray-900 mt-2 font-robotoFlex">
+                    Get The Full Picture...
+                </h1>
+            </div>
+
+            {/* Carousel */}
+        </div>
+    )
+
+}
