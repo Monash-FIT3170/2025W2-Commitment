2ac4adf79186476625c181ab11bdb9fdfc3e6294
Bailey Chessum <bche0080@student.monash.edu>
2025-05-07 15:29:06 +1000

    feat: login view

--- commitment/imports/ui/views/LoginView/LoginView.tsx
+++ commitment/imports/ui/views/LoginView/LoginView.tsx
@@ -1,11 +1,9 @@
 import React from "react";
+import LoginWidget from "@ui/components/widgets/login/LoginWidget";
 
 const LoginView = () => (
-  <div className="m-9">
-    <h1 className="text-cyan-700 font-sans text-5xl m-9 text-center font-thin">Login Page!</h1>
-    <div className="flex flex-col gap-6">
-      <p>TODO: Add a login page here!</p>
-    </div>
+  <div className="h-screen flex flex-col justify-center content-center bg-secondary">
+    <LoginWidget></LoginWidget>
   </div>
 );
 
