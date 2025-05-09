d309045981cbc1523dba290d17bf7bae10a59f6c
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-05-09 16:11:17 +1000

    fix: add background color to navbar and cropped navbar logo

--- commitment/imports/ui/components/landing-page/NavBar.tsx
+++ commitment/imports/ui/components/landing-page/NavBar.tsx
@@ -11,11 +11,11 @@ import SignUpButton from "@ui/components/ui/signUpButton";
 
 export const NavBar = () => {
   return (
-    <div className="sticky top-0 z-50 flex items-center justify-between px-4 py-2 border-b">
+    <div className="sticky top-0 z-50 flex items-center justify-between px-4 py-2 border-b bg-white rounded-md shadow-lg">
       <NavigationMenu>
         <NavigationMenuList className="flex space-x-4">
           <a className="flex items-center space-x-3">
-            <img src="/logo.png" alt="Logo" className="h-10 w-10" />
+            <img src="/logo.svg" alt="Logo" className="h-10 w-10"/>
             <span className="text-xl text-gray-900"></span>
           </a>
           <NavigationMenuItem>
