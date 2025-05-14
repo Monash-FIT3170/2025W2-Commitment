ea844a467930295948313778b8b37c0f85e4287d
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-05-14 11:52:35 +1000

    fix: Change the carousel image and some styling for the box.

--- commitment/imports/ui/components/landing-page/FeatureCarousel.tsx
+++ commitment/imports/ui/components/landing-page/FeatureCarousel.tsx
@@ -19,14 +19,14 @@ export const FeatureCard = ({
 }: FeatureCardProps) => {
   return (
     <div
-      className={`flex flex-col justify-between border-2 border-[#F1502F] bg-white rounded-xl shadow p-4 text-center h-[350px] ${className}`}
+      className={`flex flex-col justify-between border-2 border-[#F1502F] bg-white rounded-xl p-5 text-center ${className}`}
     >
       <div>
         <h2 className="text-xl font-semibold text-gray-800 mb-4">{title}</h2>
         <img
           src={image}
           alt={alt}
-          className="w-full h-48 object-cover rounded-md mb-4"
+          className="scale-50 object-cover rounded-md mb-4"
         />
       </div>
       <p className="text-gray-600 text-sm font-robotoFlex">{description}</p>
@@ -46,35 +46,35 @@ const features: Feature[] = [
     title: "Repository Analytics Dashboard",
     description:
       "View summary analytics for each Git repository, including activity levels and trends.",
-    image: "/feature-placeholder.png",
+    image: "/feature-placeholder.svg",
     alt: "Alt image.",
   },
   {
     title: "Team Comparison View",
     description:
       "Compare multiple users or groups across contribution metrics for peer review or group assessment.",
-    image: "/feature-placeholder.png",
+    image: "/feature-placeholder.svg",
     alt: "Alt image.",
   },
   {
     title: "Custom Filtering",
     description:
       "Filter analytics by time frame, contributor or branch to focus on different analytics.",
-    image: "/feature-placeholder.png",
+    image: "/feature-placeholder.svg",
     alt: "Alt image.",
   },
   {
     title: "Moodle Gradebook Integration",
     description:
       "As an educator, upload a gradebook to apply scaling based on metrics for streamlined grading.",
-    image: "/feature-placeholder.png",
+    image: "/feature-placeholder.svg",
     alt: "Alt image.",
   },
   {
     title: "Per-User Contribution Insights",
     description:
       "Track individual contributions such as commit frequency and lines of code.",
-    image: "/feature-placeholder.png",
+    image: "/feature-placeholder.svg",
     alt: "Alt image.",
   },
 ];
@@ -113,7 +113,13 @@ export function FeatureCarousel() {
   }, [emblaApi]);
 
   return (
-    <div className="relative ml-32 mr-32 mt-10 overflow-x-hidden" ref={emblaRef}>
+    <div
+      className="relative ml-32 mr-32 mt-10 overflow-x-hidden"
+      ref={emblaRef}
+    >
+
+      {/* bg-gradient-to-r from-white to-transparent */}
+
       <div className="flex">
         {features.map((feature, index) => {
           const isSelected = index === selectedIndex;
@@ -121,7 +127,9 @@ export function FeatureCarousel() {
             <div
               key={index}
               className={`ease-in-out px-2 ${
-                isSelected ? "flex-[0_0_50%] blur-0" : "flex-[0_0_50%] blur-sm"
+                isSelected
+                  ? "flex-[0_0_50%] opacity-100"
+                  : "flex-[0_0_50%] bg-gradient-to-r from-inherit to-transparent"
               }`}
             >
               <FeatureCard
