6b65c79e1c0101455f9d62b01ed67e8c1d96c4be
Amy Tjea <amycaitjea@gmail.com>
2025-05-20 00:24:25 +1000

    fix: repaired import issues caused by using absolute imports

--- commitment/imports/ui/components/widgets/dashboard/GalleryCard.tsx
+++ commitment/imports/ui/components/widgets/dashboard/GalleryCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Card, CardContent, CardHeader } from '@/imports/ui/components/ui/card'
-import { Button } from '@/imports/ui/components/ui/button';
+import { Card, CardContent, CardHeader } from '../../ui/card'
+import { Button } from '../../ui/button';
 
 interface Bookmark{
     id:number;
