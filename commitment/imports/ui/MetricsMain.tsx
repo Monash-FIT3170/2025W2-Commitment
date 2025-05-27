2530870dc2be3517c3699f31131bc78e3d429c2a
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-05-27 15:24:03 +1000

    feat: Move the top bar logic into it's own component.

--- commitment/imports/ui/MetricsMain.tsx
+++ commitment/imports/ui/MetricsMain.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { MetricsTabs } from "./components/metrics-page/MetricsTab";
+import { TopBar } from "./components/metrics-page/TopBar";
 
 export const MetricsMain = () => (
-  <div className="m-0 scroll-smooth flex flex-col gap-32">
-      <MetricsTabs />
+  <div className="m-0 scroll-smooth flex flex-col">
+    <TopBar />
+    <MetricsTabs />
   </div>
 );
