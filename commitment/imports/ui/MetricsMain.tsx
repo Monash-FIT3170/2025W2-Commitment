f573c50781d295bb734fe56693a59d2b146dd5d0
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-05-27 15:19:24 +1000

    fix: Rename test page to be the main metrics component.

--- commitment/imports/ui/MetricsMain.tsx
+++ commitment/imports/ui/MetricsMain.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { MetricsTabs } from "./components/metrics-page/MetricsTab";
 
-export const TestPage = () => (
-  <div className="m-0 scroll-smooth">
-    <div className="flex flex-col gap-32">
+export const MetricsMain = () => (
+  <div className="m-0 scroll-smooth flex flex-col gap-32">
       <MetricsTabs />
-    </div>
   </div>
 );
