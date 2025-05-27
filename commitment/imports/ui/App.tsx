70788333431b7d214620369cdaba3a818b6499a4
benmcshanag <128126812+benmcshanag@users.noreply.github.com>
2025-05-27 10:24:04 +1000

    merge conflict resolved

--- commitment/imports/ui/App.tsx
+++ commitment/imports/ui/App.tsx
@@ -2,24 +2,7 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import { LandingPage } from "@ui/LandingPage";
 import LoginView from "@ui/views/LoginView/LoginView";
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-export const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ExampleView/>}></Route>
-        <Route path="/login" element={<LoginView/>}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
-}
-=======
-
-=======
 import InsertGitRepoView from '@ui/views/InsertGitRepoView/InsertGitRepo';
->>>>>>> 98df114705301a679a1d5b13273d636d58df94be
 
 export const App = () => {
   return (
@@ -33,4 +16,3 @@ export const App = () => {
   )
 }
 
->>>>>>> b434a03c4ba4b9156a13ba04c7f121e5a4a362d2
