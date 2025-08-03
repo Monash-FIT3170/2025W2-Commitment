39f762fdae2f57d7fb4d549aa5750abee8fa1531
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-08-03 14:07:26 +1000

    feat: Playing around with using shadcn dialog.

--- commitment/imports/ui/components/scaling/ScalingView.tsx
+++ commitment/imports/ui/components/scaling/ScalingView.tsx
@@ -2,6 +2,16 @@ import React from "react";
 
 import { ScalingConfigForm } from "./ScalingConfigForm";
 import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+import { Button } from "../ui/button";
 
 interface ScalingConfig {
   metrics: string[];
@@ -10,8 +20,7 @@ interface ScalingConfig {
 }
 
 export const ScalingView = () => {
-
-    const [step, setStep] = useState<1 | 2 | 3>(1);
+  const [step, setStep] = useState<1 | 2 | 3>(1);
 
   // Shared state
   const [config, setConfig] = useState<ScalingConfig | null>(null);
@@ -21,9 +30,28 @@ export const ScalingView = () => {
     setStep(2);
   };
 
-    return(
-        <div>
-            <ScalingConfigForm onSubmit={handleConfigSubmit}/>
+  return (
+    <div className="m-0 scroll-smooth">
+      <div className="flex flex-col gap-32">
+        <div className="max-w-[1600px] mx-20 rounded-2xl bg-white p-8">
+          {/* TRIGGER FOR SCALING WIZARD */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="bg-orange-400"> Generate New Scaling</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Scaling Configuration</DialogTitle>
+              </DialogHeader>
+              <ScalingConfigForm
+                onSubmit={(data) => {
+                  console.log("Submitted Config", data);
+                }}
+              />
+            </DialogContent>
+          </Dialog>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
