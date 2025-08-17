1e9368958fa51899a524a7e4a03b4ecf96cda100
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-08-17 12:29:34 +1000

    fix: Linting for AnalyticsView.tsx file, still errors and warns to be addressed.

--- commitment/imports/ui/components/metrics-page/AnalyticsView.tsx
+++ commitment/imports/ui/components/metrics-page/AnalyticsView.tsx
@@ -1,38 +1,37 @@
-import React, { useState, useMemo, useEffect } from "react";
-import { format, subDays, addDays, isValid, set } from "date-fns";
-import { DateRange } from "react-day-picker";
-import InfoButton from "../ui/infoButton";
-import { DateRangePicker } from "./DatePickerButton";
-import { BranchDropdownMenu } from "./BranchDropdownMenu";
-import UserContributionHeatMap from "./HeatMapGraph";
-import { dark2 } from "../ui/colors";
-import { ContributorDropdownMenu } from "./ContributorDropdownMenu";
-import { HighlightCardWithGraph } from "./HighlightCard";
-import { ContributorLineGraph } from "./LineGraph";
-import { LeaderboardGraph } from "./LeaderboardGraph";
-import { ContributionPieChart } from "./PieChartGraph";
-import { topContributors } from "../../lib/utils";
-import GraphCard from "./GraphCard";
-import { useLocation } from "react-router-dom";
-import { getContributors, getBranches, getAllContributorsCommits} from "/imports/ui/components/utils/metric_functions";
-import { RepositoryData } from "/server/commitment_api/types";
-import { Subject } from "rxjs";
-import { get } from "http";
-import { getFilteredRepoData } from "../utils/data_filter";
-
-
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format, subDays, addDays, isValid, set,
+} from 'date-fns';
+import { DateRange } from 'react-day-picker';
+import { useLocation } from 'react-router-dom';
+import { Subject } from 'rxjs';
+import { get } from 'http';
+import InfoButton from '../ui/infoButton';
+import { DateRangePicker } from './DatePickerButton';
+import { BranchDropdownMenu } from './BranchDropdownMenu';
+import UserContributionHeatMap from './HeatMapGraph';
+import { dark2 } from '../ui/colors';
+import { ContributorDropdownMenu } from './ContributorDropdownMenu';
+import { HighlightCardWithGraph } from './HighlightCard';
+import { ContributorLineGraph } from './LineGraph';
+import { LeaderboardGraph } from './LeaderboardGraph';
+import { ContributionPieChart } from './PieChartGraph';
+import { topContributors } from '../../lib/utils';
+import GraphCard from './GraphCard';
+import { getContributors, getBranches, getAllContributorsCommits } from '/imports/ui/components/utils/metric_functions';
+import { RepositoryData } from '/server/commitment_api/types';
+import { getFilteredRepoData } from '../utils/data_filter';
 
 const dummyUsers = [
-  "Alice",
-  "Bob",
-  "Charlie",
-  "David",
-  "Eva",
-  "Frank",
-  "Grace",
-  "Helen",
+  'Alice',
+  'Bob',
+  'Charlie',
+  'David',
+  'Eva',
+  'Frank',
+  'Grace',
+  'Helen',
 ];
-
 
 const mockCommitLineData = [
   { value: 50 },
@@ -67,49 +66,60 @@ const mockTotalLocData = [
 ];
 
 export const mockContributorDataset = {
-  title: "Total Lines of Code",
+  title: 'Total Lines of Code',
   data: [
-    { date: "2024-01-01", Alice: 120, Bob: 90, Charlie: 100 },
-    { date: "2024-01-02", Alice: 140, Bob: 95, Charlie: 105 },
-    { date: "2024-01-03", Alice: 135, Bob: 100, Charlie: 98 },
-    { date: "2024-01-04", Alice: 160, Bob: 110, Charlie: 110 },
-    { date: "2024-01-05", Alice: 170, Bob: 120, Charlie: 115 },
-    { date: "2024-01-06", Alice: 180, Bob: 125, Charlie: 120 },
-    { date: "2024-01-07", Alice: 190, Bob: 130, Charlie: 125 },
+    {
+      date: '2024-01-01', Alice: 120, Bob: 90, Charlie: 100,
+    },
+    {
+      date: '2024-01-02', Alice: 140, Bob: 95, Charlie: 105,
+    },
+    {
+      date: '2024-01-03', Alice: 135, Bob: 100, Charlie: 98,
+    },
+    {
+      date: '2024-01-04', Alice: 160, Bob: 110, Charlie: 110,
+    },
+    {
+      date: '2024-01-05', Alice: 170, Bob: 120, Charlie: 115,
+    },
+    {
+      date: '2024-01-06', Alice: 180, Bob: 125, Charlie: 120,
+    },
+    {
+      date: '2024-01-07', Alice: 190, Bob: 130, Charlie: 125,
+    },
   ],
 };
 
 export const mockAllContributorDataset = {
-  title: "All Contributor Commits",
+  title: 'All Contributor Commits',
   data: [
-    { name: "Alice", commits: 100 },
-    { name: "Bob", commits: 80 },
-    { name: "Michael", commits: 40 },
-    { name: "Andrew", commits: 130 },
-    { name: "David", commits: 60 },
-    { name: "Tim", commits: 70 },
-    { name: "George", commits: 95 },
+    { name: 'Alice', commits: 100 },
+    { name: 'Bob', commits: 80 },
+    { name: 'Michael', commits: 40 },
+    { name: 'Andrew', commits: 130 },
+    { name: 'David', commits: 60 },
+    { name: 'Tim', commits: 70 },
+    { name: 'George', commits: 95 },
   ],
 };
 
-
-const metricsPageDescription =
-  "This page gives an overview of key metrics and performance trends.";
-
+const metricsPageDescription = 'This page gives an overview of key metrics and performance trends.';
 
 export const generateRandomContributions = (
   startDate: Date,
   endDate: Date,
-  users = dummyUsers
+  users = dummyUsers,
 ) => {
   if (!endDate || !isValid(endDate)) {
     // In the case where no end date is given
     return [];
   }
-  console.log(startDate, endDate, "both");
+  console.log(startDate, endDate, 'both');
   const data = [];
   const totalDays = Math.floor(
-    (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
+    (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24),
   );
 
   for (const user of users) {
@@ -117,12 +127,12 @@ export const generateRandomContributions = (
       const currentDate = addDays(startDate, i);
       const contributed = Math.random() < 0.45;
       const count = contributed
-        ? Math.floor(Math.pow(Math.random(), 2) * 150 + 5)
+        ? Math.floor(Math.random() ** 2 * 150 + 5)
         : 0;
 
       data.push({
         name: user,
-        date: format(currentDate, "yyyy-MM-dd"),
+        date: format(currentDate, 'yyyy-MM-dd'),
         count,
       });
     }
@@ -145,43 +155,42 @@ const transformToPieChartData = (data: any[]) => {
   }));
 };
 
-export const AnalyticsView = () => {
+export function AnalyticsView() {
   const today = new Date();
   const lastWeek = subDays(today, 6); // Last 7 days including today
-
 
   // // attempt to get the repo data from the location state
   //   // storing the repo data by calling getRepoData in metric_functions.tsx
   const location = useLocation();
-  const repoUrl = location.state?.repoUrl 
-  const [repoData, setRepoData]= useState<RepositoryData | null> (null) ;
+  const repoUrl = location.state?.repoUrl;
+  const [repoData, setRepoData] = useState<RepositoryData | null>(null);
   const [dateRange, setDateRange] = useState<DateRange | undefined>(undefined);
-  const [loading, setLoading] = useState<boolean>(true);  // Add loading state
+  const [loading, setLoading] = useState<boolean>(true); // Add loading state
   const [error, setError] = React.useState<string | null>(null);
-  const defaultDaysBack = 1000; 
+  const defaultDaysBack = 1000;
 
   useEffect(() => {
     // check if entering this useEffect
-    console.log("Entering useEffect with repoUrl:", repoUrl);
+    console.log('Entering useEffect with repoUrl:', repoUrl);
     if (!repoUrl) {
       setLoading(false);
-      console.log("No repo URL found");
+      console.log('No repo URL found');
       return;
     }
-    console.log("Starting getFilteredRepoData with URL:", repoUrl);
+    console.log('Starting getFilteredRepoData with URL:', repoUrl);
 
     const notifier = new Subject<string>();
-    const filtered = getFilteredRepoData(repoUrl, notifier, defaultDaysBack, "main");
+    const filtered = getFilteredRepoData(repoUrl, notifier, defaultDaysBack, 'main');
 
     filtered.repositoryData.then((data) => {
-        console.log("Filtered repo data fetched:", data);
-        setRepoData(data);
-        setDateRange({ from: filtered.dateRange.start, to: filtered.dateRange.end });
-        console.log("Date range being used:", filtered.dateRange);
+      console.log('Filtered repo data fetched:', data);
+      setRepoData(data);
+      setDateRange({ from: filtered.dateRange.start, to: filtered.dateRange.end });
+      console.log('Date range being used:', filtered.dateRange);
 
-        setLoading(false);
-        notifier.complete();
-      })
+      setLoading(false);
+      notifier.complete();
+    })
       .catch((e) => {
         setError(e.message);
         setLoading(false);
@@ -204,7 +213,6 @@ export const AnalyticsView = () => {
   //   [startDate, endDate]
   // );
 
-
   // const pieChartData = useMemo(() => transformToPieChartData(data), [data]);
 
   // checking if repo data is still loading
@@ -213,15 +221,20 @@ export const AnalyticsView = () => {
   }
   // checking if error has occurred
   if (error) {
-    return <div>Error: {error}</div>;
+    return (
+      <div>
+        Error:
+        {error}
+      </div>
+    );
   }
 
-  // checking if the repo data exists 
+  // checking if the repo data exists
   if (!repoData) {
     return <div>No repo data available</div>;
   }
 
-  // extracting information from metric functions 
+  // extracting information from metric functions
   const contributorData = getContributors(repoData);
   const numContributors = contributorData.length;
   const branchData = getBranches(repoData);
@@ -230,7 +243,6 @@ export const AnalyticsView = () => {
 
   // const filteredRepoData = getFilteredRepoData(repoUrl, new Subject<string>());
   // console.log("here is the filtered data", filteredRepoData);
-
 
   return (
     <div className="m-0 scroll-smooth">
@@ -260,11 +272,11 @@ export const AnalyticsView = () => {
             </div>
             <div className="flex flex-col">
               <label className="text-sm text-gray-600">Branch*</label>
-              <BranchDropDownMenu branches={branchData} />
+              <BranchDropdownMenu branches={branchData} />
             </div>
             <div className="flex flex-col">
               <label className="text-sm text-gray-600">Contributors*</label>
-              <ContributorDropDownMenu contributors={contributorData} />
+              <ContributorDropdownMenu contributors={contributorData} />
             </div>
           </div>
 
@@ -326,7 +338,7 @@ export const AnalyticsView = () => {
                 title="Total Commits"
                 value={123}
                 percentageChange={20}
-                isPositive={true}
+                isPositive
                 data={mockCommitLineData}
               />
               <HighlightCardWithGraph
@@ -366,4 +378,4 @@ export const AnalyticsView = () => {
       </div>
     </div>
   );
-};
+}
