7f2efffde01d0908c66403b905fb99c876e57af1
Nicholas Bisset <nbis0006@student.monash.edu>
2025-08-28 19:50:32 +1000

    bug fix: Haskell has leaky threads :/
    ensuring all forked logger threads are cleaned when the call is terminated (saves memory)

--- api/src/Main.hs
+++ api/src/Main.hs
@@ -3,12 +3,24 @@
 module Main (main) where
 
 import Api
+import Control.Concurrent (forkIO)
 
-import Network.Wai.Handler.Warp (runSettings, defaultSettings, setPort, setHost)
 import Network.Wai.Handler.WebSockets (websocketsOr)
 import Network.WebSockets (defaultConnectionOptions)
 
+import Network.Wai.Handler.Warp (runSettings, defaultSettings, setPort, setHost)
+import Network.Wai.Handler.Warp.Unix (runSettingsUnix, defaultUnixSettings, setUnixSocket)
+
 main :: IO ()
-main = runSettings settings $ websocketsOr defaultConnectionOptions appWS appHTTP
+main = do
+  let app = websocketsOr defaultConnectionOptions appWS appHTTP
+
+  -- Run TCP server on 0.0.0.0:8081
+  _ <- forkIO $ runSettings tcpSettings app
+
+  -- Run Unix socket server on /tmp/haskell-ipc.sock
+  runSettingsUnix unixSettings app
+
   where
-    settings = setPort 8081 $ setHost "0.0.0.0" defaultSettings
+    tcpSettings  = setPort 8081 $ setHost "0.0.0.0" defaultSettings
+    unixSettings = setUnixSocket "/tmp/haskell-ipc.sock" defaultUnixSettings
