692ae52e4ae591075f9dfe031f5665f6093fd363
Nicholas Bisset <nbis0006@student.monash.edu>
2025-08-28 20:33:46 +1000

    feat: starting the IPC architecture for more efficient API fetching
    allows the server to bypass the network layer and connect to a more responsive, faster layer that communicates with the API with Unix IPC mechanisms

--- api/src/Main.hs
+++ api/src/Main.hs
@@ -4,23 +4,38 @@ module Main (main) where
 
 import Api
 import Control.Concurrent (forkIO)
+import Control.Exception (bracket)
 
 import Network.Wai.Handler.WebSockets (websocketsOr)
 import Network.WebSockets (defaultConnectionOptions)
 
-import Network.Wai.Handler.Warp (runSettings, defaultSettings, setPort, setHost)
-import Network.Wai.Handler.Warp.Unix (runSettingsUnix, defaultUnixSettings, setUnixSocket)
+import Network.Wai.Handler.Warp (runSettings, defaultSettings, setPort, setHost, Settings)
+import Network.Wai.Handler.Warp (runSettingsSocket)
+import Network.Socket
+  ( socket, bind, listen, Socket, SockAddr(SockAddrUnix)
+  , Family(AF_UNIX), SocketType(Stream)
+  , close
+  )
 
 main :: IO ()
 main = do
-  let app = websocketsOr defaultConnectionOptions appWS appHTTP
+    let app = websocketsOr defaultConnectionOptions appWS appHTTP
 
-  -- Run TCP server on 0.0.0.0:8081
-  _ <- forkIO $ runSettings tcpSettings app
+    -- Run TCP server on 0.0.0.0:8081
+    _ <- forkIO $ runSettings tcpSettings app
 
-  -- Run Unix socket server on /tmp/haskell-ipc.sock
-  runSettingsUnix unixSettings app
+    -- Run Unix domain socket server on /tmp/haskell-ipc.sock
+    bracket (setupUnixSocket "/tmp/haskell-ipc.sock") close $ \unixSock ->
+        runSettingsSocket defaultSettings unixSock app
 
-  where
-    tcpSettings  = setPort 8081 $ setHost "0.0.0.0" defaultSettings
-    unixSettings = setUnixSocket "/tmp/haskell-ipc.sock" defaultUnixSettings
+-- TCP Settings
+tcpSettings :: Settings
+tcpSettings = setPort 8081 $ setHost "0.0.0.0" defaultSettings
+
+-- Helper to create a Unix domain socket
+setupUnixSocket :: FilePath -> IO Socket
+setupUnixSocket path = do
+    sock <- socket AF_UNIX Stream 0
+    bind sock (SockAddrUnix path)
+    listen sock 1024
+    return sock
