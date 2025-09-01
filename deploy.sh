38035cec578eb2e32401be9cfd469acc9fc08a8b
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-09-01 19:22:38 +1000

    feat: Skeleton for running deploy.

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+APP_DIR=/home/ubuntu/app
+
+# Pull latest code
+echo "Pulling latest commits!"
+if [ ! -d "$APP_DIR" ]; then
+  git clone https://github.com/Monash-FIT3170/2025W2-Commitment.git "$APP_DIR"
+else
+  cd "$APP_DIR"
+  git fetch origin main
+  git reset --hard origin/main
+fi
+
+cd "$APP_DIR"
+
+# Build and run with prod config
+echo "Building containers and running!"
+docker compose -f docker-compose.prod.yml up -d --build
+
+echo "Deployment complete!"
