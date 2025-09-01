fbea93aee154773f8f32408013c6b056a9292386
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2025-09-01 20:00:38 +1000

    feat: Change target branch.

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,14 @@ if [ ! -d "$APP_DIR" ]; then
   git clone https://github.com/Monash-FIT3170/2025W2-Commitment.git "$APP_DIR"
 else
   cd "$APP_DIR"
-  git fetch origin main
-  git reset --hard origin/main
+  git fetch origin cd/deploy
+  git reset --hard origin/cd/deploy
 fi
 
 cd "$APP_DIR"
 
 # Build and run with prod config
 echo "Building containers and running!"
-docker compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml up -d --build
 
 echo "Deployment complete!"
