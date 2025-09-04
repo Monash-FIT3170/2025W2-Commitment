d888e0dc2e0916af1d5d12df9eb588d2e16537ad
Chhordapitou Nguon <36335479+Feenerys@users.noreply.github.com>
2025-09-04 11:43:11 +1000

    feat: add Settings icon import to TopBar component

--- commitment/imports/ui/components/metrics-page/TopBar.tsx
+++ commitment/imports/ui/components/metrics-page/TopBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Settings } from 'lucide-react';
 import { Meteor } from 'meteor/meteor';
 import {  useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
