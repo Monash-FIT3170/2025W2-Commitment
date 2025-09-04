bac0518e2b90808082d77922472ab90ce96f2676
Amy Tjea <amycaitjea@gmail.com>
2025-09-04 16:10:20 +1000

    feat: fixed subtitle text on home page

--- commitment/imports/ui/views/InsertGitRepoView/InsertGitRepo.tsx
+++ commitment/imports/ui/views/InsertGitRepoView/InsertGitRepo.tsx
@@ -14,7 +14,7 @@ const InsertGitRepoView: React.FC = () => {
 
       {isLoggedIn ? (
         <div className="flex flex-col items-center pt-20">
-          <h1 className="text-6xl text-gray-700 mb-8">
+          <h1 className="text-6xl text-muted mb-8">
             Welcome Back,
             {user?.profile?.name || "User"}
           </h1>
@@ -26,11 +26,11 @@ const InsertGitRepoView: React.FC = () => {
           <div className="mb-4">
             <Logo />
           </div>
-          <p className="text-center text-lg text-gray-700 mb-8">
+          <p className="text-center text-lg text-git-text-secondary mb-8">
             Quick Git metrics for overworked TAs
           </p>
           <GitRepoInputSection />
-          <p className="text-center text-lg text-gray-700 mt-8">
+          <p className="text-center text-lg text-git-text-secondary mt-8">
             Need to quickly understand how committed your students are to their
             group? We got you
           </p>
