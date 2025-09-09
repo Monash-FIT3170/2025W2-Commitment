5fffe3ae5c870eec159592ef57b1465dbb963008
Nicholas Bisset <nbis0006@student.monash.edu>
2025-09-09 18:45:42 +1000

    feat: making redundancy for the meteorCallAsync function

--- commitment/imports/api/call_repo.ts
+++ commitment/imports/api/call_repo.ts
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import { Tracker } from 'meteor/tracker'
 import { Subject } from "rxjs"
+import { meteorCallAsync } from './meteor_interface';
 
 // Define the schema for documents in the collection
 interface PersonalServerResponse {
@@ -12,50 +13,28 @@ interface PersonalServerResponse {
 
 const ServerResponses = new Mongo.Collection<PersonalServerResponse>("fetchRepoMessagesCollection");
 
-export const fetchRepo = (url: string, subject: Subject<string>): Promise<boolean> => 
-    new Promise<boolean>((resolve, reject) => {
-			// Subscribe to your personal message stream
-			Meteor.subscribe('fetchRepoMessages')
+export const fetchRepo = (url: string, subject: Subject<string>): Promise<boolean> => {
+  // Subscribe to your personal message stream
+  Meteor.subscribe('fetchRepoMessages')
 
-			// Reactively log messages
-			Tracker.autorun(() => {
-				const messages: PersonalServerResponse[] = ServerResponses.find({}, { sort: { createdAt: -1 } }).fetch()
-				messages.forEach((m: PersonalServerResponse) => subject.next(m.text));
-			})
+  // Reactively log messages
+  Tracker.autorun(() => {
+    const messages: PersonalServerResponse[] = ServerResponses.find({}, { sort: { createdAt: -1 } }).fetch()
+    messages.forEach((m: PersonalServerResponse) => subject.next(m.text));
+  })
 
-			// Call the server method to start data retrieval
-			if (Meteor.isClient && !Meteor.status().connected) {
-				reject(new Error("Server is not found"))
-			}
-
-			Meteor.call('getGitHubRepoData', url, (err: Error, result: boolean) => {
-				if (err) reject(err)
-				resolve(result ?? false)
-			});
-		})
-
-export const repoInDatabase = async (url: string): Promise<boolean> => {
-    return new Promise<boolean>((resolve, reject) => {
-        // Only check connection status on the client side
-        if (Meteor.isClient && !Meteor.status().connected) {
-            return reject(new Error("Server is not connected"));
-        }
-
-        Meteor.call('repoInDatabase', url, (err: Error, result: boolean) => {
-            if (err) {
-                console.error('Error checking if repo is in database:', err);
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
+  return meteorCallAsync("getGitHubRepoData")(url)
 }
 
-export const getMetric = async <T>(url: string, f: string) => 
-    new Promise<T>((resolve, reject) => {
-        Meteor.call('getMetricFromRepo', url, f, (err: Error, result: T) => {
-            if (err) reject(err)    
-            resolve(result)
-        });
-    })
+export const repoInDatabase = (url: string): Promise<boolean> => {
+  // Only check connection status on the client side
+  if (Meteor.isClient && !Meteor.status().connected) {
+    throw new Error("Server is not connected")
+  }
+
+  return meteorCallAsync("repoInDatabase")(url)
+}
+
+
+export const getMetric = <T>(url: string, f: string) => 
+  meteorCallAsync<T>("getMetricFromRepo")(url, f)
--- commitment/imports/api/meteor_interface.ts
+++ commitment/imports/api/meteor_interface.ts
@@ -7,9 +7,18 @@ import { Meteor } from 'meteor/meteor'
  * @returns the value which the meteor method will return (templated for convenience)
  */
 export const meteorCallAsync = <T = any>(method: string) => (...args: any[]): Promise<T> => 
-  new Promise((resolve, reject) => 
+  new Promise((resolve, reject) => {
+    // checks if the call is from the client, if so make sure meteor is connected
+    if (Meteor.isClient && !Meteor.status().connected) 
+      reject(new Error("Server is not found"))
+    
+    // checks to see if the method is in the list of known methods
+    if (!(method in Meteor.server.method_handlers))
+      reject(new Error(`Method not found: ${method}`))
+
+    // calls the method, awaiting the result
     Meteor.call(method, ...args, (err: Error | null, res: T) => {
       if (err) reject(err)
       else resolve(res)
     })
-  ) 
+  }) 
