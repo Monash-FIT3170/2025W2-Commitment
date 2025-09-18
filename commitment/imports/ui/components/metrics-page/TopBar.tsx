37dcc67e4f19997f3199c35d66ebe60782ae5d0c
Ben McShanag <bmcs0003@student.monash.edu>
2025-09-19 07:43:49 +1000

    Fix: Bookmark loading from dashboard (#70)
    * fix: view repo button for bookmarks on dashboard
    - should now actually open the bookmarked repo
    
    * feat:  bookmark icon only shows to authenticated users
    
    ---------
    
    Co-authored-by: ben-propel <benmcshanag@propelventures.ai>
    Co-authored-by: Amy Tjea <amycaitjea@gmail.com>

--- commitment/imports/ui/components/metrics-page/TopBar.tsx
+++ commitment/imports/ui/components/metrics-page/TopBar.tsx
@@ -2,6 +2,7 @@ import React, {  useEffect , useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useLocation } from 'react-router-dom';
 import BookmarkButton from '../dashboard/BookmarkButton';
+import { useAuth } from '../../hooks/useAuth';
 
 /**
  * JANKY METHOD FOR NOW taken from chatgpt: Extracts the repository name from a Git URL
@@ -24,6 +25,7 @@ function getRepoNameFromUrl(url: string): string {
 
 export default function TopBar() {
   // call meteor method to find the name
+  const signedIn = useAuth()
   const location = useLocation();
   const repoUrl: string | null = location.state?.repoUrl ?? null;
   const [repoName, setRepoName] = useState<string>("Loading...");
@@ -48,8 +50,8 @@ export default function TopBar() {
     <div className="flex items-center justify-between px-10 py-3 border-b  border-git-stroke-primary/40 bg-git-bg-elevated">
       <div className="flex items-center gap-3">
         <h2 className="text-lg font-semibold text-gray-800">{repoName}</h2>
-        {/* Bookmark button */}
-        {repoUrl && (
+        {/* Bookmark button (only shown to signed in users) */}
+        {(repoUrl && signedIn) && (
           <BookmarkButton url={repoUrl} title={repoName} variant="secondary" />
         )}
       </div>
