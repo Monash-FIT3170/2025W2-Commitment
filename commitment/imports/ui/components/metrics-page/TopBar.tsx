9ffdfa6b53d3ec534f5a3b8b5f6a5412062b024e
Amy Tjea <amycaitjea@gmail.com>
2025-09-19 01:23:03 +1000

    feat:  bookmark icon only shows to authenticated users

--- commitment/imports/ui/components/metrics-page/TopBar.tsx
+++ commitment/imports/ui/components/metrics-page/TopBar.tsx
@@ -4,6 +4,7 @@ import { Meteor } from 'meteor/meteor';
 import { useLocation } from 'react-router-dom';
 import { AnalyticsData, Metadata } from '/imports/api/types';
 import BookmarkButton from '../dashboard/BookmarkButton';
+import { useAuth } from '../../hooks/useAuth';
 
 /**
  * JANKY METHOD FOR NOW taken from chatgpt: Extracts the repository name from a Git URL
@@ -26,6 +27,7 @@ function getRepoNameFromUrl(url: string): string {
 
 export default function TopBar() {
   // call meteor method to find the name
+  const signedIn = useAuth()
   const location = useLocation();
   const repoUrl: string | null = location.state?.repoUrl ?? null;
   const [repoName, setRepoName] = useState<string>("Loading...");
@@ -50,8 +52,8 @@ export default function TopBar() {
     <div className="flex items-center justify-between px-10 py-3 border-b  border-git-stroke-primary/40 bg-git-bg-elevated">
       <div className="flex items-center gap-3">
         <h2 className="text-lg font-semibold text-gray-800">{repoName}</h2>
-        {/* Bookmark button */}
-        {repoUrl && (
+        {/* Bookmark button (only shown to signed in users) */}
+        {(repoUrl && signedIn) && (
           <BookmarkButton url={repoUrl} title={repoName} variant="secondary" />
         )}
       </div>
