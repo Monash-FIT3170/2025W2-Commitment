4b31fce36c7e989438a509525cf28d2314a79688
Nicholas Bisset <nbis0006@student.monash.edu>
2025-09-18 10:52:10 +1000

    bug fix: fixing methods nested meteor call

--- commitment/server/methods.ts
+++ commitment/server/methods.ts
@@ -12,10 +12,7 @@ import {
   Selections,
   AllMetricsData,
   MetricType,
-  RepositoryData,
 } from "../imports/api/types";
-
-import { getContributors } from "./helper_functions";
 
 import { getAllGraphData, getAllMetricsFromData } from "./repo_metrics";
 import { applyAliasMappingIfNeeded } from "./alias_mapping";
@@ -31,13 +28,7 @@ Meteor.methods({
    * @param params.contributor Contributor to filter (optional)
    * @returns FilteredData structure
    */
-  async "repo.getFilteredData"({
-    repoUrl,
-    startDate,
-    endDate,
-    branch,
-    contributor,
-  }: {
+  async "repo.getFilteredData"(a: {
     repoUrl: string; // pass the URl from the frontend
     startDate: Date;
     endDate: Date;
@@ -46,53 +37,18 @@ Meteor.methods({
   }): Promise<FilteredData> {
     // TODO do type checks here if needed
 
-    // Get full repository data from db (fetches from API if not updated or in the database)
-    const repo: SerializableRepoData = await getSerialisedRepoData(repoUrl, null);
-
-    // Apply alias mapping if user has config
-    const mappedRepo = await applyAliasMappingIfNeeded(repo, this.userId || "");
-
-    // Apply filtering
-    const filteredData = getFilteredRepoDataServer(
-      repoUrl,
-      startDate,
-      endDate,
-      mappedRepo,
-      branch,
-      contributor
-    );
-    return filteredData;
+    return getFilteredData({
+      ...a,
+      userId: this.userId,
+    });
   },
 
   async "repo.getMetadata"(repoUrl: string): Promise<Metadata> {
-    // Get full repository data from db
-    const repo: SerializableRepoData = await getSerialisedRepoData(repoUrl, null);
-
-    // Apply alias mapping if user has config
-    const mappedRepo = await applyAliasMappingIfNeeded(repo, this.userId || "");
-
-    return {
-      repoUrl,
-      repoName: mappedRepo.name,
-      branches: mappedRepo.branches.map((b) => b.branchName),
-      contributors: mappedRepo.contributors.map((c) => c.key),
-      dateRange: {
-        from: new Date(
-          Math.min(...mappedRepo.allCommits.map((c) => new Date(c.value.timestamp).getTime()))
-        ),
-        to: new Date(),
-      },
-    };
+    // TODO do type checks here if needed
+    return getMetaData(repoUrl, this.userId);
   },
 
-  async "repo.getAnalyticsData"({
-    repoUrl,
-    startDate,
-    endDate,
-    branch,
-    contributors,
-    metric,
-  }: {
+  async "repo.getAnalyticsData"(d: {
     repoUrl: string;
     startDate?: Date;
     endDate?: Date;
@@ -100,54 +56,12 @@ Meteor.methods({
     contributors?: string[];
     metric: MetricType;
   }): Promise<AnalyticsData> {
-    /**
-     * Get Repo Metadata first (contributors, branches, date range) etc
-     *
-     * Then get the filtered data depending on the parameters that have been passed
-     *
-     * Run our metrics functions on the filtered data
-     *
-     * Return the full AnalyticsData structure
-     */
+    // TODO do type checks here if needed
 
-    // Get project metadata
-    const metadata: Metadata = await Meteor.callAsync("repo.getMetadata", repoUrl);
-
-    const selections: Selections = {
-      selectedBranch:
-        branch ??
-        (metadata.branches.includes("main")
-          ? "main"
-          : metadata.branches.includes("master")
-          ? "master"
-          : metadata.branches[0]),
-      selectedContributors:
-        !contributors || contributors.length === 0 ? metadata.contributors : contributors,
-      selectedMetrics: metric,
-      selectedDateRange: {
-        from: startDate || metadata.dateRange.from,
-        to: endDate || metadata.dateRange.to,
-      },
-    };
-
-    const filteredRepo: FilteredData = await Meteor.callAsync("repo.getFilteredData", {
-      repoUrl,
-      startDate: selections.selectedDateRange.from,
-      endDate: selections.selectedDateRange.to,
-      branch: selections.selectedBranch,
-      contributor: selections.selectedContributors,
+    return getAnalyticsData({
+      ...d,
+      userId: this.userId,
     });
-
-    const metricsData: MetricsData = await getAllGraphData(filteredRepo, metric);
-
-    // NOW WE DO STUFF WITH THE FILTERED REPO TO GET the specific metric!!
-    const returnData: AnalyticsData = {
-      metadata,
-      selections,
-      metrics: metricsData,
-    };
-
-    return returnData;
   },
 
   /**
@@ -162,16 +76,136 @@ Meteor.methods({
     const mappedRepo = await applyAliasMappingIfNeeded(repo, this.userId || "");
 
     // Use the mapped data for metrics calculation
-    return await getAllMetricsFromData(mappedRepo);
+    return getAllMetricsFromData(mappedRepo);
   },
 
   async getScalingResults(data: ScalingConfig, repoUrl: string) {
-    const n = new Subject<string>();
     return getScaledResults(
-      await tryFromDatabaseSerialised(repoUrl, n),
+      await tryFromDatabaseSerialised(repoUrl, null),
       data,
       repoUrl,
       "" // null string for now as Yoonus is TODO fix this
     );
   },
 });
+
+export const getFilteredData = async ({
+  repoUrl,
+  startDate,
+  endDate,
+  branch,
+  contributor,
+  userId,
+}: {
+  repoUrl: string; // pass the URl from the frontend
+  startDate: Date;
+  endDate: Date;
+  branch?: string;
+  contributor?: string[];
+  userId?: string;
+}) => {
+  // Get full repository data from db (fetches from API if not updated or in the database)
+  const repo: SerializableRepoData = await getSerialisedRepoData(repoUrl, null);
+
+  // Apply alias mapping if user has config
+  const mappedRepo = await applyAliasMappingIfNeeded(repo, userId || "");
+
+  // Apply filtering
+  const filteredData = getFilteredRepoDataServer(
+    repoUrl,
+    startDate,
+    endDate,
+    mappedRepo,
+    branch,
+    contributor
+  );
+  return filteredData;
+};
+
+export const getMetaData = async (repoUrl: string, userId?: string): Promise<Metadata> => {
+  // Get full repository data from db
+  const repo: SerializableRepoData = await getSerialisedRepoData(repoUrl, null);
+
+  // Apply alias mapping if user has config
+  const mappedRepo = await applyAliasMappingIfNeeded(repo, userId || "");
+
+  return {
+    repoUrl,
+    repoName: mappedRepo.name,
+    branches: mappedRepo.branches.map((b) => b.branchName),
+    contributors: mappedRepo.contributors.map((c) => c.key),
+    dateRange: {
+      from: new Date(
+        Math.min(...mappedRepo.allCommits.map((c) => new Date(c.value.timestamp).getTime()))
+      ),
+      to: new Date(),
+    },
+  };
+};
+
+export const getAnalyticsData = async ({
+  repoUrl,
+  startDate,
+  endDate,
+  branch,
+  contributors,
+  metric,
+  userId,
+}: {
+  repoUrl: string;
+  startDate?: Date;
+  endDate?: Date;
+  branch?: string;
+  contributors?: string[];
+  metric: MetricType;
+  userId?: string;
+}): Promise<AnalyticsData> => {
+  /**
+   * Get Repo Metadata first (contributors, branches, date range) etc
+   *
+   * Then get the filtered data depending on the parameters that have been passed
+   *
+   * Run our metrics functions on the filtered data
+   *
+   * Return the full AnalyticsData structure
+   */
+
+  // Get project metadata
+  const metadata: Metadata = await getMetaData(repoUrl, userId);
+
+  const selections: Selections = {
+    selectedBranch:
+      branch ??
+      (metadata.branches.includes("main")
+        ? "main"
+        : metadata.branches.includes("master")
+        ? "master"
+        : metadata.branches[0]),
+    selectedContributors:
+      !contributors || contributors.length === 0 ? metadata.contributors : contributors,
+    selectedMetrics: metric,
+    selectedDateRange: {
+      from: startDate || metadata.dateRange.from,
+      to: endDate || metadata.dateRange.to,
+    },
+  };
+
+  const filteredRepo: FilteredData = await getFilteredData({
+    repoUrl,
+    startDate: selections.selectedDateRange.from,
+    endDate: selections.selectedDateRange.to,
+    branch: selections.selectedBranch,
+    contributor: selections.selectedContributors,
+  });
+
+  const metricsData: MetricsData = await getAllGraphData(filteredRepo, metric);
+
+  // NOW WE DO STUFF WITH THE FILTERED REPO TO GET the specific metric!!
+  const returnData: AnalyticsData = {
+    metadata,
+    selections,
+    metrics: metricsData,
+  };
+
+  return returnData;
+};
