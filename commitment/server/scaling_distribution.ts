296b7e4e196414f18b09a6f412f7e7f616f4a677
Milni Abeysekara <milniabeysekara04@gmail.com>
2025-09-24 17:05:49 +1000

    fix: added documentation for current functions.

--- commitment/server/scaling_distribution.ts
+++ commitment/server/scaling_distribution.ts
@@ -1,7 +1,3 @@
-// This file will consist of functions that returns the desired input for the contributor metric-scaling distribution graph
- 
-// function to produce contributor metrics 
-// need to use get filtered data as the input to do so . 
 import {ContributorMetrics, ContributorScaledData, ContributorScaledMetric, FilteredData, MetricType, SerialisableMapObject, ContributorData } from "../imports/api/types"
 import { getCommitPerDayPerContributor, getTotalCommitsPerContributor, getLOCperContributor, getLocPerCommitPerContributor } from "./helper_functions";
 
@@ -28,19 +24,27 @@ export function getContributorMetrics(data: FilteredData): ContributorMetrics[]{
 
 /** 
  * This function provides the percentile of a contributors metric.
+ * @param passedContributor the contributor to get the percentile for
+ * @param data the filtered data
+ * @param selectedMetric the metric to get the percentile for
+ * @returns ContributorScaledMetric a data structure containing the metric, value and percentile
  */
-export function getContributorScaledMetric(passedContributor: SerialisableMapObject<string, ContributorData>, data: FilteredData, selectedMetric: MetricType): ContributorScaledMetric {    // get all contributor metrics 
+export function getContributorScaledMetric(passedContributor: SerialisableMapObject<string, ContributorData>, data: FilteredData, selectedMetric: MetricType): ContributorScaledMetric {   
     // get all contributor metrics
     const contributorMetrics = getContributorMetrics(data);
 
     // get the metric values for all contributors 
     const metricValues = contributorMetrics.map((contributor) => contributor.metrics[selectedMetric]);
+    
     // get the value for the passed contributor 
     const contributorName = passedContributor.value.name;
+   
+    // find the contributor in the metrics array
     const contributorMetric = contributorMetrics.find((contributor) => contributor.contributorName === contributorName);
     if (!contributorMetric) {
         throw new Error(`Contributor ${contributorName} not found in metrics.`);
     }
+    // get the contributor's value for the selected metric
     const contributorValue = contributorMetric.metrics[selectedMetric];
 
     // calculate the number of values below the contributor's value
@@ -49,6 +53,7 @@ export function getContributorScaledMetric(passedContributor: SerialisableMapObj
     
     // calculate the percentile
     const percentile = (numValuesBelow / totalValues) * 100;
+
     return {
         metric: selectedMetric,
         value: contributorValue,
@@ -57,10 +62,15 @@ export function getContributorScaledMetric(passedContributor: SerialisableMapObj
     
 }
 
+/**
+ * This function provides the scaled data for each contributor in the filtered data.
+ * @param data the filtered data
+ * @param selectedMetric the metric to get the scaled data for
+ * @returns an array of ContributorScaledData
+ */
 export function getContributorScaledData(data: FilteredData, selectedMetric:MetricType): ContributorScaledData[]{
-    // this would iterate through each contributor and getContributorScaledMetric for each contributor
     const { repositoryData, repositoryData: { contributors } } = data;
-
+    
     return contributors.map((contributor) => {
         const scaledMetric = getContributorScaledMetric(contributor, data, selectedMetric);
         return {
