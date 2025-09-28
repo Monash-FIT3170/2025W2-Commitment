b80e5e7164774f5209885af902483ba001fd5f5b
Bailey Chessum <bailey.chessum1@gmail.com>
2025-09-29 05:07:40 +1000

    test: found execution time limit loophole

--- python_executor/sandbox/test.py
+++ python_executor/sandbox/test.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+import subprocess
 
-print("Counting to 100!")
 i = 0
 while True:
-  print(i+1)
+  result = subprocess.run(
+    ["python3", "-c", f"print({i + 1} ** 2.5)"],          # Command as a list
+    capture_output=True,   # Capture stdout and stderr
+    text=True              # Return output as a string instead of bytes
+  )
+  exec(f"print(\"Got output: {result.stdout.strip()} {result.stderr.strip()}\")")
   i+=1
 print("Done!")
