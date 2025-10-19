373120ad60d2072c7b0f83c7c17a5b5984a9fc63
Bailey Chessum <bailey.chessum1@gmail.com>
2025-10-19 11:27:20 +1100

    feat: simplify package layout

--- python_executor/default.nix
+++ python_executor/default.nix
@@ -7,7 +7,6 @@
 let
   wrap = import ./nix/wrap { inherit pkgs lib; };
   fhs = pkgs.callPackage ./nix/fhs.nix { inherit pkgs wrap; };
-  executor-python = pkgs.callPackage ./executor { inherit lib fhs; };
-  python-executor = pkgs.callPackage ./executor/python-executor.nix { inherit lib fhs; };
+  python-executor = pkgs.callPackage ./executor { inherit lib fhs; };
 in
   python-executor
