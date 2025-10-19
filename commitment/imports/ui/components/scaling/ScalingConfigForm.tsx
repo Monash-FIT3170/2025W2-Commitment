b57712a26b818b184a8c03fc2733a6bf870e1fbe
YoonusNazz <mnaz0012@student.monash.edu>
2025-10-19 13:27:19 +1100

    update: updating max contributor size for compact scaling

--- commitment/imports/ui/components/scaling/ScalingConfigForm.tsx
+++ commitment/imports/ui/components/scaling/ScalingConfigForm.tsx
@@ -93,7 +93,7 @@ function ScalingConfigForm({ onSubmit }: ScalingConfigFormProps) {
         const result = (await Meteor.callAsync(
           "isSmallContributorGroup",
           repoUrl,
-          6
+          7
         )) as boolean;
 
         // update cache
