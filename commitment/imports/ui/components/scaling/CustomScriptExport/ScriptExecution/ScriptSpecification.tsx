aa865f5bffb53b355a13d89c1d45c995896a9053
Bailey Chessum <bailey.chessum1@gmail.com>
2025-10-20 17:44:33 +1100

    wip: high level component for specifying script to execute

---
+++ commitment/imports/ui/components/scaling/CustomScriptExport/ScriptExecution/ScriptSpecification.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import {useLocalStorage} from "@hook/useLocalStorage";
+
+const initial_script = `
+import os
+import csv
+
+print("This is an example script output")
+
+print("If you print a List or Tuple, it will be added to the data field returned by the API.")
+print(("Joe", 0.8))
+print(["John", 1.12])
+
+print("Displaying all files in current directory:")
+current_dir = os.getcwd()
+files = [f for f in os.listdir(current_dir) if os.path.isfile(os.path.join(current_dir, f))]
+
+# Print each file
+for f in files:
+  print(f)
+
+print("Getting data from the csv:")
+with open("./data.csv", newline="", encoding="utf-8") as data_csv:
+  data = csv.reader(data_csv)
+  for row in data:
+    # each row is a list of strings
+    print(row)
+`;
+
+export default function ScriptSpecification() {
+  const [code, setCode] = useLocalStorage('custom-execution-script', initial_script);
+
+
+  return (
+    <div>
+      TODO -- script upload / editor
+
+    </div>
+  )
+}
